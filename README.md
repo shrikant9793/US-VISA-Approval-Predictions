4c7ee015172d590e81e466626f7261f5269ed052
shrikant9793 <shrinaths2@gmail.com>
2024-02-21 22:36:48 +0530

    intalled required library

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # US-VISA-Approval-Predictions
+
+# How to run?
+
+'''bash
+conda create -n visa python=3.8 -y
+
+conda activate visa
+
+
+
+'''
---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name= "us_visa",
+    version="0.0.0",
+    author="shrikant9793",
+    author_email="shrinaths2@gmail.com",
+    packages = setuptools.find_packages(where = "src")
+)
